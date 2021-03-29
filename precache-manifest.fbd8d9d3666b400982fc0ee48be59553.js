c28c68afa549b8925f748e02710e50749720f4fc
iHaveSticks <60411065+iHaveSticks@users.noreply.github.com>
2021-03-28 18:11:30 -0700

    Updates

---
+++ precache-manifest.fbd8d9d3666b400982fc0ee48be59553.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9bb0a1545e9dcc10478259415763d6f",
+    "url": "/Super-Cool-Clicker-Idle/index.html"
+  },
+  {
+    "revision": "d40e9c3d755bff2e1b39",
+    "url": "/Super-Cool-Clicker-Idle/static/css/main.823271da.chunk.css"
+  },
+  {
+    "revision": "94fcf82360ccec8bd409",
+    "url": "/Super-Cool-Clicker-Idle/static/js/2.df67f60b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Super-Cool-Clicker-Idle/static/js/2.df67f60b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d40e9c3d755bff2e1b39",
+    "url": "/Super-Cool-Clicker-Idle/static/js/main.1c7d6262.chunk.js"
+  },
+  {
+    "revision": "d723a418a75c88544263",
+    "url": "/Super-Cool-Clicker-Idle/static/js/runtime-main.2ab5e551.js"
+  }
+]);

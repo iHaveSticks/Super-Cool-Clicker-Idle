43bac07d546b05b9ac431689615afc6b51eb79c9
iHaveSticks <60411065+iHaveSticks@users.noreply.github.com>
2021-03-30 15:36:43 -0700

    Changed GameStats class components to a functional compnent

--- src/components/GameStats/GameStats.js
+++ src/components/GameStats/GameStats.js
@@ -4,19 +4,15 @@ import NumberCompacter from './../../functions/number-compacter.js';
 
 
 
-class GameStats extends React.Component {
-    render() {
-        return(
-            <div style={GameStatStyles}>
-                <h3>Stats</h3>
-                    <p style={rowStyles}>Per autoclick:&nbsp;{NumberCompacter(this.props.amountPerAutoClick/(this.props.autoClickSpeed/1000))}/s</p>
-                    <p style={rowStyles}>Per selfclick:&nbsp;{NumberCompacter(this.props.amountPerClick)}</p>
-                    <p style={rowStyles}>Total Clicks:&nbsp;{NumberCompacter(this.props.clicksTotal)}</p>
-                    <br />
-                    <p style={rowStyles}>Pinecones:&nbsp;{NumberCompacter(this.props.pineconesCurrent)}</p>
-            </div>
-        )
-    }
+export default function GameStats(props)  {
+    return(
+        <div style={GameStatStyles}>
+            <h3>Stats</h3>
+                <p style={rowStyles}>Per autoclick:&nbsp;{NumberCompacter(props.amountPerAutoClick/(props.autoClickSpeed/1000))}/s</p>
+                <p style={rowStyles}>Per selfclick:&nbsp;{NumberCompacter(props.amountPerClick)}</p>
+                <p style={rowStyles}>Total Clicks:&nbsp;{NumberCompacter(props.clicksTotal)}</p>
+                <br />
+                <p style={rowStyles}>Pinecones:&nbsp;{NumberCompacter(props.pineconesCurrent)}</p>
+        </div>
+    )
 }
-
-export default GameStats;

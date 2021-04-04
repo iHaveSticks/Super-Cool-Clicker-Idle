1fc1e3cc13c2e708da3ac5cdb45a4f53ce9946a1
iHaveSticks <60411065+iHaveSticks@users.noreply.github.com>
2021-04-04 14:42:45 -0700

    Improved numberCompacter()
    Changed numberCompactor function to add commas for numbers in thousands range
    
    Reduced repetitive code

--- src/functions/number-compacter.js
+++ src/functions/number-compacter.js
@@ -1,82 +1,95 @@
 
 
 export default function NumberCompacter(num) {
-        // Changes a long inputed number into a shorter form starting from millions.
-        // ex int 3784587 changes to '3.78 Million'
+        /*
+                Changes number over a thousand into a shorter form
+                or adds a comma if in the thousands range
 
+                EX: int 3784587 changes to '3.78 Million'
+                EX: int 1000 changes to '1,000'
+                EX: int 222 stays the same
+        */
+        num = Math.abs(Number(num)).toFixed(0);
 
         // fifty-seven Zeroes for Octodecillions
-        return Math.abs(Number(num)) >= 1.0e+57
-        ? (Math.abs(Number(num)) / 1.0e+57).toFixed(2) + " Octodecillion"
+        return num >= 1.0e+57
+        ? (num / 1.0e+57).toFixed(2) + " Octodecillion"
 
         // Fifty-four Zeroes for Septendecillions
-        : Math.abs(Number(num)) >= 1.0e+54
-        ? (Math.abs(Number(num)) / 1.0e+54).toFixed(2) + " Septendecillion"
+        : num >= 1.0e+54
+        ? (num / 1.0e+54).toFixed(2) + " Septendecillion"
 
         // Fifty-one Zeroes for Sexdecillions
-        : Math.abs(Number(num)) >= 1.0e+51
-        ? (Math.abs(Number(num)) / 1.0e+51).toFixed(2) + " Sexdecillion"
+        : num >= 1.0e+51
+        ? (num / 1.0e+51).toFixed(2) + " Sexdecillion"
 
         // Forty-eight Zeroes for Tredecillions
-        : Math.abs(Number(num)) >= 1.0e+48
-        ? (Math.abs(Number(num)) / 1.0e+48).toFixed(2) + " Quindecillion"
+        : num >= 1.0e+48
+        ? (num / 1.0e+48).toFixed(2) + " Quindecillion"
 
         // Forty-five Zeroes for Quattuordecillions
-        : Math.abs(Number(num)) >= 1.0e+45
-        ? (Math.abs(Number(num)) / 1.0e+45).toFixed(2) + " Quattuordecillion"
+        : num >= 1.0e+45
+        ? (num / 1.0e+45).toFixed(2) + " Quattuordecillion"
 
         // Forty-two Zeroes for Tredecillions
-        : Math.abs(Number(num)) >= 1.0e+42
-        ? (Math.abs(Number(num)) / 1.0e+42).toFixed(2) + " Tredecillion"
+        : num >= 1.0e+42
+        ? (num / 1.0e+42).toFixed(2) + " Tredecillion"
 
         // Thirty-nine Zeroes for Duodecillions
-        : Math.abs(Number(num)) >= 1.0e+39
-        ? (Math.abs(Number(num)) / 1.0e+39).toFixed(2) + " Duodecillion"
+        : num >= 1.0e+39
+        ? (num / 1.0e+39).toFixed(2) + " Duodecillion"
 
         // Thirty-six Zeroes for Undecillions
-        : Math.abs(Number(num)) >= 1.0e+36
-        ? (Math.abs(Number(num)) / 1.0e+36).toFixed(2) + " Undecillion"
+        : num >= 1.0e+36
+        ? (num / 1.0e+36).toFixed(2) + " Undecillion"
 
         // Thirty-three Zeroes for Decillions
-        : Math.abs(Number(num)) >= 1.0e+33
-        ? (Math.abs(Number(num)) / 1.0e+33).toFixed(2) + " Decillion"
+        : num >= 1.0e+33
+        ? (num / 1.0e+33).toFixed(2) + " Decillion"
 
         // Thirty Zeroes for Nonillions
-        : Math.abs(Number(num)) >= 1.0e+30
-        ? (Math.abs(Number(num)) / 1.0e+30).toFixed(2) + " Nonillion"
+        : num >= 1.0e+30
+        ? (num / 1.0e+30).toFixed(2) + " Nonillion"
 
         // Twenty-seven Zeroes for Octillions
-        : Math.abs(Number(num)) >= 1.0e+27
-        ? (Math.abs(Number(num)) / 1.0e+27).toFixed(2) + " Octillion"
+        : num >= 1.0e+27
+        ? (num / 1.0e+27).toFixed(2) + " Octillion"
 
         // Twenty-four Zeroes for Septillions
-        : Math.abs(Number(num)) >= 1.0e+24
-        ? (Math.abs(Number(num)) / 1.0e+24).toFixed(2) + " Septillion"
+        : num >= 1.0e+24
+        ? (num / 1.0e+24).toFixed(2) + " Septillion"
 
         // Twenty-one Zeroes for Sextillions
-        : Math.abs(Number(num)) >= 1.0e+21
-        ? (Math.abs(Number(num)) / 1.0e+21).toFixed(2) + " Sextillion"
+        : num >= 1.0e+21
+        ? (num / 1.0e+21).toFixed(2) + " Sextillion"
 
         // Eighteen Zeroes for Quintillions
-        : Math.abs(Number(num)) >= 1.0e+18
-        ? (Math.abs(Number(num)) / 1.0e+18).toFixed(2) + " Quintillion"
+        : num >= 1.0e+18
+        ? (num / 1.0e+18).toFixed(2) + " Quintillion"
 
         // Fifteen Zeroes for Quadrillions
-        : Math.abs(Number(num)) >= 1.0e+15
-        ? (Math.abs(Number(num)) / 1.0e+15).toFixed(2) + " Quadrillion"
+        : num >= 1.0e+15
+        ? (num / 1.0e+15).toFixed(2) + " Quadrillion"
 
         // Twelve Zeroes for Trillions
-        : Math.abs(Number(num)) >= 1.0e+12
-        ? (Math.abs(Number(num)) / 1.0e+12).toFixed(2) + " Trillion"
+        : num >= 1.0e+12
+        ? (num / 1.0e+12).toFixed(2) + " Trillion"
 
         // Nine Zeroes for Billions
-        : Math.abs(Number(num)) >= 1.0e+9
-        ? (Math.abs(Number(num)) / 1.0e+9).toFixed(2) + " Billion"
+        : num >= 1.0e+9
+        ? (num / 1.0e+9).toFixed(2) + " Billion"
 
         // Six Zeroes for Millions 
-        : Math.abs(Number(num)) >= 1.0e+6
-        ? (Math.abs(Number(num)) / 1.0e+6).toFixed(2) + " Million"
+        : num >= 1.0e+6
+        ? (num / 1.0e+6).toFixed(2) + " Million"
     
-        : Math.abs(Number(num).toFixed(0))
-    
+        // Three Zeroes for Thousands
+        : num >= 1.0e+3
+        ?  // Slice string to add comma and then rejoin
+         num.toString().slice(0, num.toString().length - 3)
+         + ',' +
+         num.toString().slice(num.toString().length - 3)
+
+        // No change
+        : num;
 }
